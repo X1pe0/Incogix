9619487ce381eb5e91f495d3630438d2e63fec4c
xsyncd <mewsiof@gmail.com>
2021-01-12 07:01:44 -0700

    reimplement venv using the correct version this time
    got rid of other unused dependencies too.

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Still working on it, however it works.
 ![Alt text](https://github.com/X1pe0/Incogix/blob/main/22e8f786156c09254133929ffbd0a8e27753a531Capture.PNG "Image")
 
 
+## If you come across any erros try the following
+
+if you're getting the error `FileNotFoundError: [Errno 2] No such file or directory: b'liblibc.a'`
+
+Try: `cd /usr/lib/x86_64-linux-gnu/
+ln -s -f libc.a liblibc.a`
+or `sudo ln -s  /usr/lib/libc.a /usr/lib/liblibc.a`
--- scan/IOT_Scanner.py
+++ scan/IOT_Scanner.py
@@ -1,7 +1,4 @@
-import os
-import subprocess 
 from netaddr import iter_iprange
-import threading
 from scapy.all import *
 
 ## Do not make thread limit more then the IP range. ##
@@ -16,26 +13,29 @@ threadLock = threading.Lock()
 with threadLock:
     # Supporting non-standard addresses is probably not necessary, revise as needed
     generator = iter_iprange(IP_range_Start, IP_range_Stop, step=1)
-    
+
 
 def scanit(num):
-     
     for ips in generator:
-        print ('Worker: %s Scanning - %s'%(num,ips))
+        print('Worker: %s Scanning - %s' % (num, ips))
         response = os.system("ping -w 1 -c 1 " + str(ips) + " > scan.log")
 
         if response == 0:
-            if not os.path.exists((str(ips))+".html"):
-                stdout = subprocess.getoutput("nmap --max-rtt-timeout 2 --max-scan-delay 1 --max-retries 1 --version-intensity 0 -sV --script=banner,vuln -sU -sT -p%s-%s --min-rate 100000 -oX - "%(port_range_start,port_range_stop) + str(ips))
-                with open(str(ips)+".scan",'w') as f: f.write(stdout)
-                stdout = subprocess.getoutput("xsltproc %s.scan -o %s.html"%(ips,ips))
-                os.system("cp %s.scan ./raw/%s.scan_raw.txt"%(ips,ips))
-                os.system("rm %s.scan"%(ips))
+            if not os.path.exists((str(ips)) + ".html"):
+                stdout = subprocess.getoutput(
+                    "nmap --max-rtt-timeout 2 --max-scan-delay 1 --max-retries 1 --version-intensity 0 -sV --script=banner,vuln -sU -sT -p%s-%s --min-rate 100000 -oX - " % (
+                        port_range_start, port_range_stop) + str(ips))
+                with open(str(ips) + ".scan", 'w') as f:
+                    f.write(stdout)
+                stdout = subprocess.getoutput("xsltproc %s.scan -o %s.html" % (ips, ips))
+                os.system("cp %s.scan ./raw/%s.scan_raw.txt" % (ips, ips))
+                os.system("rm %s.scan" % (ips))
             else:
                 pass
 
         else:
             pass
+
 
 threads = []
 
